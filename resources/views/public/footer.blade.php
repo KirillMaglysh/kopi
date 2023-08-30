98834e4c6b7ebdecf7c03db67fd36961a36e9079
KirillMaglysh <kirillmaglys@gmail.com>
2023-08-31 00:16:44 +0300

    UI fix

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-top pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Поддержать</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Наши мечтатели</a></li>
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link px-2 text-muted">Авторизация</a></li>
         <li class="nav-item"><a href="{{ route('register') }}" class="nav-link px-2 text-muted">Регистрация</a></li>
     </ul>
--- resources/views/public/menu.blade.php
+++ resources/views/public/menu.blade.php
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('support')  }}">Поддержать</a>
+                    <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('support')  }}">Наши мечтатели</a>
                 </li>
                 <li class="nav-item mx-0 mx-lg-1">
                     <a class="nav-link py-3 px-0 px-lg-3 rounded" href="{{ route('login')  }}">Авторизация</a>
